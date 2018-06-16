a1c85a314a1149dcf29ab5e16fcb9701394a9b03
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-16 16:45:27 +0530

    Set up bird flapping

---
+++ Flappy Bird 2D/Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/joyDivided13
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+	private bool _isDead = false;
+	private Rigidbody2D _rb2d;
+	
+	public float upForce = 200f;
+	
+	void Start ()
+	{
+		_rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () 
+	{
+		if(_isDead == false)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				_rb2d.velocity = Vector2.zero; //removing any existing velocity, cartoon physics
+				_rb2d.AddForce(new Vector2(0, upForce));
+			}
+		}
+	}
+
+	void OnCollisionEnter2D() //spelling matters
+	{
+		_isDead = true;
+	}
+}
