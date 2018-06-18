a9dd08afbbf08dce2122965045d1bcb94232b35f
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-18 20:02:02 +0530

    Set up the column score mechanism
    The score text now responds when you successfully clear a column

--- Flappy Bird 2D/Assets/Scripts/GameControl.cs
+++ Flappy Bird 2D/Assets/Scripts/GameControl.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
@@ -14,6 +15,9 @@ public class GameControl : MonoBehaviour
 	public GameObject gameOverText;
 	public float scrollSpeed = -1.5f;
 	public bool gameOver = false;
+	private int _score;
+	public Text scoreText;
+
 	void Awake ()
 	{
 		if(instance == null)
@@ -32,6 +36,17 @@ public class GameControl : MonoBehaviour
 		{
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //reloading the scene
 		}
+	}
+
+	public void BirdScored()
+	{
+		if(gameOver)
+		{
+			return;
+		}
+
+		_score++;
+		scoreText.text = "Score: " + _score.ToString();
 	}
 
 	public void BirdDied()
