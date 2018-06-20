39b80f59dac235784b34017fa5de8879889f511b
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-21 02:45:22 +0530

    Changed the quality settings

--- Flappy Bird 2D/Assets/Scripts/Sound.cs
+++ Flappy Bird 2D/Assets/Scripts/Sound.cs
@@ -14,7 +14,6 @@ public class Sound		//the sound class yhat stores data of the sounds
 	public bool loop;
 	[Range(0f, 1f)]
 	public float volume;
-	[Range(.1f, .3f)]
 	[HideInInspector]
 	public AudioSource source;
 }
